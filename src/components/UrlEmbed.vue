683e46c984335f785ef30648d63e01e4ffbabdd5
Kevin Thompson <phlux@ewnix.net>
2023-08-19 10:12:10 -0500

    This doesn't work, but I need to push it to dev to find out why

--- src/components/UrlEmbed.vue
+++ src/components/UrlEmbed.vue
@@ -1,60 +1,32 @@
 <template>
-    <div class="kiwi-mediaviewer-embedly">
-        <div v-html="embedHtml"></div>
+    <div class="url-embed" v-if="response">
+        <div v-html="response.html"></div>
     </div>
 </template>
 
 <script>
-'kiwi public';
-
 import axios from 'axios';
 
 export default {
-    props: ['url', 'showPin', 'iframeSandboxOptions'],
+    props: {
+        url: {
+            type: String,
+            required: true,
+        },
+    },
     data() {
         return {
-            embedHtml: '',
+            response: null,
         };
     },
-    computed: {
-        iframelyApiKey() {
-            return this.$state.setting('iframelyApiKey');
-        },
-
-        iframelyApiUrl() {
-            return `https://iframe.ly/api/iframely?url=${this.url}&api_key=${this.iframelyApiKey}&iframe=card`;
-        },
-    },
-    watch: {
-        url() {
-            this.getEmbed();
-        },
-    },
-    created() {
-        this.getEmbed();
-    },
-    methods: {
-        async getEmbed() {
-            try {
-                const response = await axios.get(this.iframelyApiUrl);
-                if (response.data && response.data.html) {
-                    this.embedHtml = response.data.html;
-                }
-            } catch (error) {
-                console.error('Error fetching embed code:', error);
-            }
-        },
+    async mounted() {
+        try {
+            const apiKey = this.$state.setting('iframelyApiKey');
+            const result = await axios.get(`https://iframe.ly/api/iframely?url=${this.url}&api_key=${apiKey}`);
+            this.response = result.data;
+        } catch (error) {
+            console.error('Error fetching embed:', error);
+        }
     },
 };
 </script>
-
-<style>
-.kiwi-mediaviewer-embedly {
-    display: inline-block;
-    overflow: auto;
-}
-
-.kiwi-main-mediaviewer .kiwi-mediaviewer-embedly {
-    display: block;
-}
-</style>
